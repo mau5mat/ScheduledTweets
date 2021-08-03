4f0d3e03c3376f2f5bafcdcc8157cd5b8e633883
mau5mat <mau5mat@gmail.com>
2021-08-03 22:53:40 +0100

    Added main+controller as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # GET /about/ page
-  get "about", to: "about#index"
-  
+  get "/about", to: "about#index"
+
+  # GET root
+  get "/", to: "main#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

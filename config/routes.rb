36fde9f236a4e75c3c23b7668a0aac403540c8ea
mau5mat <mau5mat@gmail.com>
2021-08-03 23:47:06 +0100

    Small change to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # GET /about/ page
   get "/about", to: "about#index"
 
-  # GET root
-  get "/", to: "main#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "main#index"
 end

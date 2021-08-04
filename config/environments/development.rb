ac7f4e3d9658f4becb92ecff9bfdcbaff7032c89
mau5mat <mau5mat@gmail.com>
2021-08-04 19:26:41 +0100

    Added url_helpers in _navbar file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Show changes in HTML without restarting server
+  config.reload_classes_only_on_change = false
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
